cef49965afc3bfe5b8eb698c8700c6a84fbf2ee4
Matthijs Laan <matthijsln@gmail.com>
2022-02-10 10:12:18 +0100

    Fix lint

--- projects/app/src/app/app.module.ts
+++ projects/app/src/app/app.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CoreModule, CoreRoutingModule } from "@tailormap-viewer/core";
-import { SharedModule } from "@tailormap-viewer/shared";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { CoreModule, CoreRoutingModule } from '@tailormap-viewer/core';
+import { SharedModule } from '@tailormap-viewer/shared';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
@@ -18,6 +18,6 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
     SharedModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
