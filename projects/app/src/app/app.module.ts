d38fd7ce326fe3409866e45a57888ef755ab8fe4
Geert Plaisier <geert.plaisier@gnamic.nl>
2023-08-31 09:43:52 +0200

    Revert "Merge branch 'main' into main"
    This reverts commit 7884c9d16405a3ff3c72397697fc406207e61474, reversing
    changes made to 62466f442321045fff18be900b98b62981a2e133.

--- projects/app/src/app/app.module.ts
+++ projects/app/src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CoreModule, CoreRoutingModule, MapControlsService } from "@tailormap-viewer/core";
+import { CoreModule, CoreRoutingModule } from "@tailormap-viewer/core";
 import { SharedModule } from "@tailormap-viewer/shared";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { LogoOnMapComponent } from './logo-on-map/logo-on-map.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LogoOnMapComponent
   ],
   imports: [
     BrowserModule,
@@ -22,10 +20,4 @@ import { LogoOnMapComponent } from './logo-on-map/logo-on-map.component';
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {
-  constructor(
-    mapControlsService: MapControlsService,
-  ) {
-    mapControlsService.registerComponent(LogoOnMapComponent);
-  }
-}
+export class AppModule { }
